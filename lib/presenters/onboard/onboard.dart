81b5ec8b3141ffca6ed6b87effe0c6c910009335
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-05-09 21:47:48 -0300

    fix: fix fonts path

---
+++ lib/presenters/onboard/onboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatefulWidget {
+  const Onboard({super.key});
+
+  @override
+  State<Onboard> createState() => _OnboardState();
+}
+
+class _OnboardState extends State<Onboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Onboard'),
+    );
+  }
+}
